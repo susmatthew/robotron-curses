64580c751aa6239a0f0a19632a7b42719fc3ec11
susmatthew <matthew@suspectdeviceaudio.com>
2017-02-02 22:49:55 -0500

    ASCII robotron initial commit

---
+++ robocurse.c
@@ -0,0 +1,28 @@
+/*
+
+// robotron 2084 clone for linux written in C 
+// Using ncurses and attempting to parse /dev
+// for sweet WASD-ing. 
+
+// today, ASCII robotron. Tomorrow, OpenGL?
+
+// the Linux kernel style guide speaks to me, so I used that.
+
+// started 2.2.17
+
+*/
+
+void main ()
+{
+	while (1) {
+	// game loop:
+	// check environment (screen size 80x24)
+	// call engine:
+	//	controls
+	// 	collisions
+	//	scoring
+	// enemy logic
+	// player logic
+	// render the screen
+	}	
+}
